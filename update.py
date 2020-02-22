2c10d8ddd4cfc6eb49d0ffce999f149ed3343a01
szafranski <szawus@gmail.com>
2020-02-22 16:46:56 +0100

    Update update.py

--- update.py
+++ update.py
@@ -665,8 +665,8 @@ def mainMenu():
 					print("""\n\n\t\t
 		Aliases in Linux act like shortcuts or referances to another commands. You can use them every time when you \n\t
 		operates in the terminal window. For example instead of typing 'python ~/RotorHazard/src/server/server.py' \n\t
-		you can just type 'ss' (server start) etc. Aliases can be modified and added anytime you want. You just have to \n\t
-		open '~./bashrc' file with text editor like 'nano'. After that you have reboot or paste 'source ~/.bashrc' \n\t
+		you can just type 'ss' (server start) etc. Aliases can be modified and added anytime you want. You just \n\t  
+		have to open '~./bashrc' file in text editor like 'nano'. After that you have reboot or type 'source ~/.bashrc' \n\t
 		- or use alias 'rld'. \n\n\t
 	       Alias			Command					What it does	\n
 		ss 	-->  python ~/RotorHazard/src/server/server.py   # starts the server\n\t
@@ -678,7 +678,9 @@ def mainMenu():
 		ota  	-->  python ~/RH-ota/update.py  		 # opens updating script\n\t
 		als  	-->  nano ~/.bashrc   				 # opens this file\n\t
 		rld  	-->  source ~/.bashrc   			 # reloads aliases file \n\t
-		rcfg  	-->  sudo raspi-config   			 # open raspberry's configs\n\t\n
+		rcfg  	-->  sudo raspi-config   			 # open raspberry's configs\n\t\n\n
+		
+		Do you want to use above aliases in your system?
 		""")
 					
 					selection=str(raw_input("\t\t\t\t\t\t"+bcolors.YELLOW+"Press 'y' for yes or 'a' for abort"+bcolors.ENDC+"\n"))
