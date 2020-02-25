a583baeb554a0faa9e7c58b618f82615f5a10a6e
szafranski <23621888+szafranski@users.noreply.github.com>
2020-02-25 03:22:02 +0100

    Update net_and_ap.py

--- net_and_ap.py
+++ net_and_ap.py
@@ -22,6 +22,16 @@ else:
 
 myPlace = data['country']
 
+class bcolors:
+	HEADER = '\033[95m'
+	ORANGE = '\033[33m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 def image():
 	print("""
@@ -80,12 +90,36 @@ first()
 
 print("""Step 1.""")
 
+def stepZero():
+	sleep(0.12)
+	os.system("clear")
+	sleep(0.12)
+	logoTop()
+	sleep(0.12)
+	print("""\n\n\t\tAfter performing this process your Raspberry Pi can be used as standalone\n
+		Access Point. You won't need additional router for connecting with it. \n
+		You will loose ability of connecting Raspberry wirelessly to any router or hotspot.\n
+		You will still have ability to connect it to the Internet sharing device, \n
+		like router or PC via ethernet cable. You will also be able to connect with the timer\n
+		via Wifi from PC or mobile phone etc. - if you had range. \n\n
+		This process will require few reboots. Do you want to continue?\n""")
+	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Yes by pressing 'y' """+bcolors.ENDC+"""\n\n\t\t\t\t"""
+	+bcolors.YELLOW+"""    Exit by pressing 'e'"""+bcolors.ENDC+"""\n""")
+	selection=str(raw_input(""))
+	if selection=='y':
+		stepOne()
+	if selection=='e':
+		sys.exit()
+	else :
+		main()
+
 def stepOne():
 	os.system("sudo sed -i 's/country/# country/g' /etc/wpa_supplicant/wpa_supplicant.conf")
 	os.system("echo 'country="+myPlace+"'| sudo  tee -a /boot/config.txt")
 	os.system("sudo apt-get update && sudo apt-get upgrade -y")
+	os.system("sudo apt install curl -y")
 	os.system("curl -sL https://install.raspap.com | bash -s -- -y")
-	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\t\t\t\t"""
+	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\n\t\t\t\t"""
 			+bcolors.YELLOW+"""    Exit by pressing 'e'"""+bcolors.ENDC+"""\n""")
 	selection=str(raw_input(""))
 	if selection=='r':
@@ -123,7 +157,7 @@ save settings
 
 DON'T CHANGE OTHER SETTINGS IN GUI!  """)
 
-	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\t\t\t\t"""
+	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\n\t\t\t\t"""
 			+bcolors.YELLOW+"""    Exit by pressing 'e'"""+bcolors.ENDC+"""\n""")
 	selection=str(raw_input(""))
 	if selection=='r':
@@ -157,7 +191,7 @@ def stepThree():
 	os.system("echo 'dhcp-range=172.20.20.21,172.20.20.255,255.255.255.0,24h' | sudo tee -a /etc/dnsmasq.conf")
 	os.system("sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.my")
 
-	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\t\t\t\t"""
+	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\n\t\t\t\t"""
 			+bcolors.YELLOW+"""    Exit by pressing 'e'"""+bcolors.ENDC+"""\n""")
 	selection=str(raw_input(""))
 	if selection=='r':
@@ -181,7 +215,7 @@ Now you should be able to enter the network typing in the browser:
 10.10.10.10:5000 - using WiFi
 172.20.20.20:5000 - using ethernet.""")
 
-	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\t\t\t\t"""
+	print("""\n\t\t\t\t"""+bcolors.GREEN+"""    Reboot by pressing 'r' """+bcolors.ENDC+"""\n\n\t\t\t\t"""
 			+bcolors.YELLOW+"""    Exit by pressing 'e'"""+bcolors.ENDC+"""\n""")
 	selection=str(raw_input(""))
 	if selection=='r':
@@ -237,6 +271,7 @@ def netConf():
 		netConf()
 
 def main():
+	stepZero()
 	stepOne()
 	stepTwo()
 	stepThree()
