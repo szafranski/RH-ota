48ded2a6d6f0010fc8f10d2e1babb1f245719aeb
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-03 08:25:45 +0100

    Update self.py

--- self.py
+++ self.py
@@ -1,26 +1,18 @@
 from time import sleep
 import os
 import sys
-import json
-
-# with open('~/RH-ota/updater-config.json') as config_file:
-	# data = json.load(config_file)
-# if data['debug_mode'] == 1:
-	# linux_testing = True
-# else:
-	# linux_testing = False 
-
-# if linux_testing == True:
-	# user = data['debug_user']
-# else:
-	# user = data['pi_user']
+#import json
 
 if os.path.exists("~/RH-ota/updater-config.json") == True:
+	config_file_exists = True
+
+if config_file_exists == True:
 	os.system("sudo cp ~/RH-ota/updater-config.json ~/.ota_markers/updater-config.json")
 os.system("sudo rm -r ~/RH-ota")
 os.system("git clone --depth=1 https://github.com/szafranski/RH-ota.git") 
-os.system("sudo cp ~/.ota_markers/updater-config.json ~/RH-ota/updater-config.json")
-print("\n\n\t\t RotorHazard OTA Manager updated - you can see 'update-notes.txt'\n\n")
+if config_file_exists == True:
+	os.system("sudo cp ~/.ota_markers/updater-config.json ~/RH-ota/updater-config.json")
+print("\n\n\n\t\t RotorHazard OTA Manager updated - you can see 'update-notes.txt'\n\n\n")
 sleep(2)
 
 
