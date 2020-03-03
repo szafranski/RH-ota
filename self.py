7d45225aa6f5d2641b6f42204f3e1fe8627e7046
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-03 08:54:32 +0100

    Update self.py

--- self.py
+++ self.py
@@ -12,16 +12,10 @@ else:
 
 if config_file_exists == True:
 	os.system("sudo cp ~/RH-ota/updater-config.json ~/.ota_markers/updater-config.json")
-	print("jest!")
-else:
-	print("brak")
 os.system("sudo rm -r ~/RH-ota")
 os.system("git clone --depth=1 https://github.com/szafranski/RH-ota.git") 
 if config_file_exists == True:
 	os.system("sudo cp ~/.ota_markers/updater-config.json ~/RH-ota/updater-config.json")
-	print("jest2!")
-else:
-	print("brak")
 print("\n\n\n\t\t RotorHazard OTA Manager updated - you can see 'update-notes.txt'\n\n\n")
 sleep(2)
 
