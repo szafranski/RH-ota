a502ffe081cf1f8a5572f1cdee613084a8cce144
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-04 06:54:19 +0100

    Update i2c_nodes_update.py

--- i2c_nodes_update.py
+++ i2c_nodes_update.py
@@ -1,4 +1,26 @@
 from smbus import SMBus
+from time import sleep
+import os
+import sys
+import json
+
+if os.path.exists("./updater-config.json") == True:
+	with open('updater-config.json') as config_file:
+		data = json.load(config_file)
+else:
+	with open('distr-updater-config.json') as config_file:
+		data = json.load(config_file)
+
+preffered_RH_version = data['RH_version']
+
+if preffered_RH_version == 'master':
+	firmware_version = 'master'
+if preffered_RH_version == 'beta':
+	firmware_version = 'beta'
+if preffered_RH_version == 'stable':
+	firmware_version = 'stable'
+if preffered_RH_version == 'custom':
+	firmware_version = 'stable'
 
 addr = 0x08 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
@@ -7,8 +29,15 @@ def main():
 	selection=str(raw_input("What do you want to send?"))
 	if selection=='1':
 		bus.write_byte(addr, 0x1) # switch it on
+	if selection=='0':
+		bus.write_byte(addr, 0x0) # switch it on
 	if selection=='2':
 		bus.write_byte(addr, 0x0) # switch it on
+		sleep(0.5)
+		bus.write_byte(addr, 0x1) # switch it on
+		os.system("sudo avrdude -v -p atmega328p -c arduino -P /dev/ttyS0 -b 57600 -U flash:w:/home/"+user+"/RH-ota/firmware/"+firmware_version+"/blink.hex:i ")
+		print("\n\t Node 1 flashed\n")
+		sleep(1.5)
 	main()
 main()
 
