7564c22590b873b963e36fe0917bee4bbce088a6
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-06 06:38:31 +0100

    Update self.py

--- self.py
+++ self.py
@@ -9,15 +9,25 @@ if os.path.exists(homedir+"/RH-ota/updater-config.json") == True:
 else:
 	config_file_exists = False
 
-def versionCheck():
-	os.system("grep 'soft_version =' ~/RH-ota/update.py > ~/.ota_markers/.version")
-	os.system("sed -i 's/soft_version = //' ~/.ota_markers/.version")
-	os.system("sed -i 's/#.*/ /' ~/.ota_markers/.version")
-	f = open(homedir+"/.ota_markers/.version","r")
+def oldVersionCheck():
+	os.system("grep 'soft_version =' ~/RH-ota/update.py > ~/.ota_markers/.old_version")
+	os.system("sed -i 's/soft_version = //' ~/.ota_markers/.old_version")
+	os.system("sed -i 's/#.*/ /' ~/.ota_markers/.old_version")
+	f = open(homedir+"/.ota_markers/.old_version","r")
 	for line in f:
-		global version_name
-		version_name = line
+		global old_version_name
+		old_version_name = line
 
+def newVersionCheck():
+	os.system("grep 'soft_version =' ~/RH-ota/update.py > ~/.ota_markers/.new_version")
+	os.system("sed -i 's/soft_version = //' ~/.ota_markers/.new_version")
+	os.system("sed -i 's/#.*/ /' ~/.ota_markers/.new_version")
+	f = open(homedir+"/.ota_markers/.new_version","r")
+	for line in f:
+		global new_version_name
+		new_version_name = line
+
+oldVersionCheck()
 print("\n\n\n\t\t Updating process has been started...\n\n\n")
 if config_file_exists == True:
 	os.system("sudo cp ~/RH-ota/updater-config.json ~/.ota_markers/updater-config.json")
@@ -25,8 +35,8 @@ os.system("sudo rm -r ~/RH-ota")
 os.system("git clone --depth=1 https://github.com/szafranski/RH-ota.git") 
 if config_file_exists == True:
 	os.system("sudo cp ~/.ota_markers/updater-config.json ~/RH-ota/updater-config.json")
-versionCheck()
-print("\n\n\n\t RotorHazard OTA Manager updated to version "+version_name+"\n\t\tYou may check update-notes.txt.\n\n\n")
+newVersionCheck()
+print("\n\n\n\t RotorHazard OTA Manager updated from "+old_version_name+" to "+new_version_name+"\n\t\tYou may check update-notes.txt.\n\n\n")
 sleep(2)
 
 
