8602192da22aaf8d4b948239c5c5d9d09d92c710
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-06 06:42:23 +0100

    Update self.py

--- self.py
+++ self.py
@@ -13,7 +13,6 @@ def oldVersionCheck():
 	os.system("grep 'soft_version =' ~/RH-ota/update.py > ~/.ota_markers/.old_version")
 	os.system("sed -i 's/soft_version = //' ~/.ota_markers/.old_version")
 	os.system("sed -i 's/#.*/ /' ~/.ota_markers/.old_version")
-	os.system("sed -i 's/ //' ~/.ota_markers/.old_version")
 	f = open(homedir+"/.ota_markers/.old_version","r")
 	for line in f:
 		global old_version_name
@@ -23,14 +22,13 @@ def newVersionCheck():
 	os.system("grep 'soft_version =' ~/RH-ota/update.py > ~/.ota_markers/.new_version")
 	os.system("sed -i 's/soft_version = //' ~/.ota_markers/.new_version")
 	os.system("sed -i 's/#.*/ /' ~/.ota_markers/.new_version")
-	os.system("sed -i 's/ //' ~/.ota_markers/.new_version")
 	f = open(homedir+"/.ota_markers/.new_version","r")
 	for line in f:
 		global new_version_name
 		new_version_name = line
 
 oldVersionCheck()
-print("\n\n\n\t\t Updating process has been started...\n\n\n")
+print("\n\n\n\t\t Updating process from version"+old_version_name+"has been started...\n\n\n")
 if config_file_exists == True:
 	os.system("sudo cp ~/RH-ota/updater-config.json ~/.ota_markers/updater-config.json")
 os.system("sudo rm -r ~/RH-ota")
@@ -38,7 +36,7 @@ os.system("git clone --depth=1 https://github.com/szafranski/RH-ota.git")
 if config_file_exists == True:
 	os.system("sudo cp ~/.ota_markers/updater-config.json ~/RH-ota/updater-config.json")
 newVersionCheck()
-print("\n\n\n\t RotorHazard OTA Manager updated from "+old_version_name+" to "+new_version_name+"\n\t\tYou may check update-notes.txt.\n\n\n")
+print("\n\n\n\t RotorHazard OTA Manager updated to version "+new_version_name+"\n\t\tYou may check update-notes.txt.\n\n\n")
 sleep(2)
 
 
