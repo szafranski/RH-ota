697f8546ee090a1ae9127bc9eccbceeef44bfcc0
szafranski <23621888+szafranski@users.noreply.github.com>
2020-03-08 11:33:09 +0100

    compatibility as another file

--- update.py
+++ update.py
@@ -8,7 +8,7 @@ import sys
 import json
 import subprocess
 
-updater_version = '2.2.8h'  ### version of THIS program - has nothing to do with the RH version
+updater_version = '2.2.8i'  ### version of THIS program - has nothing to do with the RH version
                             ### it reffers to the API level of newest contained nodes firmware 
                             ### third number reffers to actual verion of the updater itself
 
@@ -123,59 +123,7 @@ def logoUpdate():
 		\n\n""")
 
 def compatibility():               ### adds compatibility and fixes with previous versions
-	if os.path.exists(homedir+"/.ota_markers") == False:
-		os.system("mkdir "+homedir+"/.ota_markers")
-	if os.path.exists(homedir+"/.aliases_added") == True:
-		if os.path.exists(homedir+"/.ota_markers/.aliases_added") == False:
-			os.system("cp "+homedir+"/.aliases_added "+homedir+"/.ota_markers/.aliases_added ")
-		os.system("rm "+homedir+"/.aliases_added")
-	if os.path.exists(homedir+"/.updater_self") == True:
-		if os.path.exists(homedir+"/.ota_markers/.aliases_added") == False:
-			os.system("cp "+homedir+"/.updater_self "+homedir+"/.ota_markers/.updater_self ")
-		os.system("rm "+homedir+"/.updater_self")
-	if os.path.exists(homedir+"/.old_RotorHazard.old/.installation-check_file.txt") == True:
-		if os.path.exists(homedir+"/.ota_markers/.installation-check_file.txt") == False:
-			os.system("cp /home/"+user+"/.old_RotorHazard.old/.installation-check_file.txt "+homedir+"/.ota_markers/.installation-check_file.txt")
-		os.system("rm "+homedir+"/.installation-check_file.txt")
-	if os.path.exists(homedir+"/.serialok") == True:
-		if os.path.exists(homedir+"/.ota_markers/.serialok") == False:
-			os.system("cp "+homedir+"/.serialok "+homedir+"/.ota_markers/.serialok")
-		os.system("rm "+homedir+"/.serialok")
-	if os.path.exists(homedir+"/.bashrc") == True:
-		if check_if_string_in_file(homedir+'/.bashrc', 'RotorHazard OTA Manager updated'):
-			os.system("sed -i 's/alias updateupdater/# alias updateupdater/g' "+homedir+"/.bashrc")
-			os.system("sed -i 's/RotorHazard OTA Manager updated/old alias/g' "+homedir+"/.bashrc")
-			os.system("echo 'alias updateupdater=\"cd ~ && sudo cp ~/RH-ota/self.py ~/.ota_markers/self.py && sudo python ~/.ota_markers/self.py \"  # part of self-updater' | tee -a ~/.bashrc >/dev/null")
-		if check_if_string_in_file(homedir+'/.bashrc', 'starts the server'):
-			os.system("sed -i 's/alias ss/# alias ss/g' "+homedir+"/.bashrc")
-			os.system("sed -i 's/starts the server/old alias/g' "+homedir+"/.bashrc")
-			os.system("echo 'alias ss=\"cd ~/RotorHazard/src/server && python server.py\"   #  starts the RH-server' | tee -a ~/.bashrc >/dev/null")
-		if check_if_string_in_file(homedir+'/.bashrc', 'opens updating script'):
-			os.system("sed -i 's/alias ota=/# alias ota=/g' "+homedir+"/.bashrc")
-			os.system("sed -i 's/opens updating script/old alias/g' "+homedir+"/.bashrc")
-			os.system("echo 'alias ota=\"cd ~/RH-ota && python update.py\"  # opens updating soft' | tee -a ~/.bashrc >/dev/null")
-		if check_if_string_in_file(homedir+'/.bashrc', 'part of self-updater'):
-			os.system("sed -i 's/part of self-updater/part of self updater/g' "+homedir+"/.bashrc")
-			os.system("echo 'alias uu=\"cd ~ && cp ~/RH-ota/self.py ~/.ota_markers/self.py && python ~/.ota_markers/self.py \"  # part of self updater' | tee -a ~/.bashrc >/dev/null")
-		if os.path.exists(homedir+"/.ota_markers/.aliases_added") == True:
-			if os.path.exists(homedir+"/.ota_markers/.aliases2_added") == False:
-				os.system("echo 'alias otacfg=\"sudo nano ~/RH-ota/updater-config.json \"  # opens updater conf. file' | tee -a ~/.bashrc >/dev/null")
-				os.system("echo 'alias otacpcfg=\"cd ~/RH-ota && sudo cp distr-updater-config.json updater-config.json \"  # copies ota conf. file' | tee -a ~/.bashrc >/dev/null")
-				os.system("echo 'alias home=\"cd ~ \"  # go homedir (without ~ sign)' | tee -a ~/.bashrc >/dev/null")
-				os.system("echo 'functionality added - leave file here' | tee -a ~/.ota_markers/.aliases2_added >/dev/null")
-		if os.path.exists("./updater-config.json") == True:
-			if not check_if_string_in_file(homedir+'/RH-ota/updater-config.json', '"pins_assignment"'):
-				os.system("sed -i 's/\"debug_mode\" : 0/\"debug_mode\" : 0,/g' "+homedir+"/RH-ota/updater-config.json")
-				os.system("sed -i 's/\"debug_mode\" : 1/\"debug_mode\" : 1,/g' "+homedir+"/RH-ota/updater-config.json")
-				os.system("sed -i 's/}/	\"pins_assignment\" : \"default\" /g' "+homedir+"/RH-ota/updater-config.json")
-				os.system("echo '}' | tee -a "+homedir+"/RH-ota/updater-config.json >/dev/null 2>&1")
-			if check_if_string_in_file(homedir+'/RH-ota/updater-config.json', '"pins_assignment"'):
-				if not check_if_string_in_file(homedir+'/RH-ota/updater-config.json', '"updates_without_pdf"'):
-					os.system("sed -i 's/\"pins_assignment\" : \"default\"/\"pins_assignment\" : \"default\",/g' "+homedir+"/RH-ota/updater-config.json")
-					os.system("sed -i 's/\"pins_assignment\" : \"custom\"/\"pins_assignment\" : \"custom\",/g' "+homedir+"/RH-ota/updater-config.json")
-					os.system("sed -i 's/\"pins_assignment\" : \"PCB\"/\"pins_assignment\" : \"PCB\",/g' "+homedir+"/RH-ota/updater-config.json")
-					os.system("sed -i 's/}/	\"updates_without_pdf\" : 0 /g' "+homedir+"/RH-ota/updater-config.json")
-					os.system("echo '}' | tee -a "+homedir+"/RH-ota/updater-config.json >/dev/null 2>&1")
+	os.system("python ./prev_comp.py")
 
 #		if check_if_string_in_file(homedir+'/.bashrc', 'rld'):
 #			rldals.communicate()
@@ -265,8 +213,8 @@ def aliasesMenu():
 		os.system("echo 'alias rld=\"source ~/.bashrc\"   #  reloads aliases file' | tee -a ~/.bashrc")
 		os.system("echo 'alias rcfg=\"sudo raspi-config\"   #  open raspberrys configs' | tee -a ~/.bashrc")
 		os.system("echo 'alias gitota=\"git clone --depth=1 https://github.com/szafranski/RH-ota.git\"   #  clones ota repo' | tee -a ~/.bashrc")
-		os.system("echo 'alias otacfg=\"sudo nano ~/RH-ota/updater-config.json \"  # opens updater conf. file' | tee -a ~/.bashrc")
-		os.system("echo 'alias otacpcfg=\"cd ~/RH-ota && sudo cp distr-updater-config.json updater-config.json \"  # copies ota conf. file' | tee -a ~/.bashrc")
+		os.system("echo 'alias otacfg=\"nano ~/RH-ota/updater-config.json \"  # opens updater conf. file' | tee -a ~/.bashrc")
+		os.system("echo 'alias otacpcfg=\"cd ~/RH-ota && cp distr-updater-config.json updater-config.json \"  # copies ota conf. file' | tee -a ~/.bashrc")
 		os.system("echo 'alias home=\"cd ~ \"  # go homedir (without ~ sign)' | tee -a ~/.bashrc")
 		os.system("echo '' | tee -a ~/.bashrc")
 		os.system("echo '# After adding or changing aliases manually - reboot raspberry or type \"source ~/.bashrc\".' | tee -a ~/.bashrc")
@@ -294,8 +242,8 @@ def aliasesMenu():
 	rld   	 -->  source ~/.bashrc   			 # reloads aliases file \t
 	rcfg   	 -->  sudo raspi-config   			 # open raspberry's configs\t
 	gitota 	 -->  git clone https://github.com/sza(...) 	 # clones ota repo\t
-	otacfg   -->  sudo nano ~/RH-ota/updater-config.json 	 # opens updater conf. file\t
-	otacpcfg -->  cd (...) sudo cp distr (...) config.json 	 # copies ota cfg.\t
+	otacfg   -->  nano ~/RH-ota/updater-config.json 	 # opens updater conf. file\t
+	otacpcfg -->  cd (...)  cp distr (...) config.json 	 # copies ota cfg.\t
 	home	 -->  cd ~ 	 				 # go homedir (without ~ sign)\t\t\n
 	"""+bcolors.ENDC+"""
 		\tDo you want to use above aliases in your system?\n\t
