4265aabfe46902749331d359bd4dfa07f89cdb38
szafranski <p.fabiszewski@gmail.com>
2020-03-10 14:45:52 +0100

    new functions and fixes

--- .auto_public.sh
+++ .auto_public.sh
@@ -1,13 +1,15 @@
 git checkout master &&
 git pull &&
+git fetch &&
 git checkout no_pdf_included && 
+git pull &&
+git fetch &&
 git merge master -m "new functions and fixes" && 
 git pull &&
-git push &&
 git add . &&
+git push
 git commit -m "new functions and fixes" -a && 
 git push && 
-git fetch &&
 git checkout master && 
 git merge no_pdf_included --no-commit --no-ff  no_pdf_included -m "new functions and fixes" && 
 git pull &&
