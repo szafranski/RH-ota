ba6d3665fabcf45a3f7644ed52b5c85e5ec67e01
szafranski <p.fabiszewski@gmail.com>
2020-03-11 16:04:32 +0100

    features and fixes

--- .public.sh
+++ .public.sh
@@ -1,11 +1,11 @@
 git add .
 git commit -m "features and fixes"
 git push
-cp ~/RH-ota ~/RH-ota-bup
+cp -r ~/RH-ota ~/RH-ota-bup
 git checkout no_pdf_included
 cd ~
-rm -r ~/RH-ota-bup/.git
-cp ~/RH-ota-bup ~/RH-ota
+rm -rf ~/RH-ota-bup/.git
+cp ~/RH-ota-bup/* ~/RH-ota
 cd RH-ota
 rm -r how_to
 git add .
