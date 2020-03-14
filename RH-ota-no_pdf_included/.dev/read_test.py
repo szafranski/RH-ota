7359e6a442489406876a7d63f42db0a89792b831
szafranski <p.fabiszewski@gmail.com>
2020-03-14 22:34:27 +0100

    new functions and fixes

--- RH-ota-no_pdf_included/.dev/read_test.py
+++
@@ -1,8 +0,0 @@
-
-with open('./net_ap/net_steps.txt', 'rt') as f:
-	for line in f:
-		if '### step2' in line:
-			for line in f:
-				print line.replace('\n', '').replace('####', '')
-				if '####' in line:
-					break
--- RH-ota-no_pdf_included/.dev/server_start.sh
+++
@@ -1,3 +0,0 @@
-printf "Server booting, please wait...\n\n"
-cd ~/RotorHazard/src/server
-python server.py
