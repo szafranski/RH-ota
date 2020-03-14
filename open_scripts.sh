cb59dea5906c747e6b37e634eb3dc88f09d3f145
szafranski <p.fabiszewski@gmail.com>
2020-03-14 23:15:02 +0100

    new functions and fixes

--- open_scripts.sh
+++ open_scripts.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+dots5()
+{
+	for i in 1 2 3 4 5
+	do
+		printf "."
+		sleep 0.08
+	done
+	printf "\n\n"
+}
+
+
+
 server_start () 
 {
-#	printf "Server booting, please wait...\n\n" | pv -qL 10
-	printf "Server booting, please wait...\n\n"
+	printf "Server booting, please wait"
+	dots5
 	cd ~/RotorHazard/src/server
 	python server.py
 }
@@ -16,7 +28,7 @@ configuraton_start ()
 
 net_check()
 {
-rm index* > /dev/null 2>&1
+	rm index* > /dev/null 2>&1
 #[ "$(ping -c 2 8.8.8.8 | grep '100% packet loss' )" != "" ]
 timeout 10s wget www.google.com
 #sleep 2
@@ -25,12 +37,12 @@ timeout 10s wget www.google.com
 
 dots20()
 {
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-do
-printf "."
-sleep 0.08
-done
-printf "\n\n"
+	for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+		do
+			printf "."
+			sleep 0.08
+		done
+	printf "\n\n"
 }
 
 updater_from_ota()
