7a95205a02d8be459c4bf7fa7da8523c5fc2a5d7
szafranski <p.fabiszewski@gmail.com>
2020-03-14 15:56:57 +0100

    update

--- open_scripts.sh
+++ open_scripts.sh
@@ -23,6 +23,33 @@ timeout 10s wget www.google.com
 #exit
 }
 
+updater_agent()
+{
+while True
+{
+if update_now_FLAG == True
+then
+{
+printf "sudo kill -2  $(pidof python update.py)\n"
+sleep 1
+sudo kill -2  $(pidof python update.py) 
+sleep 1
+printf "\n\nUpdating process will be started soon...\n\n"
+sleep 1
+sleep 5
+cd ~ 
+printf "cp ~/RH-ota/self.py ~/.ota_markers/self.py\n"
+sleep 1
+cp ~/RH-ota/self.py ~/.ota_markers/self.py 
+printf "timeout 20 python ~/.ota_markers/self.py\n"
+sleep 1
+timeout 20 python ~/.ota_markers/self.py
+}
+fi
+}
+
+}
+
 updater_from_ota()
 {
 printf "sudo kill -2  $(pidof python update.py)\n"
