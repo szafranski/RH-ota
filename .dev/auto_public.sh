63dffd138e2b266a7b8778c9264d03e876d81efe
szafranski <p.fabiszewski@gmail.com>
2020-03-15 00:22:37 +0100

    commits changed

--- .dev/auto_public.sh
+++ .dev/auto_public.sh
@@ -2,11 +2,11 @@ git pull
 cd ~/RH-ota
 git status
 git add .
-git commit -m "new functions and fixes" -a
+git commit -a
 git push
 git checkout no_pdf_included
 git status
-git merge master -m "updated with master"
+git merge master -m "up-to-date with master"
 git push
 git checkout master
 
