703d96e637d36e82fc3fb8a21ab167943623faa1
szafranski <p.fabiszewski@gmail.com>
2020-03-16 22:56:54 +0100

    config file addedwith configparser options

--- pifind.py
+++ pifind.py
@@ -1,24 +1,26 @@
 from time import sleep 
 import os
-
-selection = str(raw_input("Do you have 'xterm' installed? [hit 'n' if not or 'Enter' to dismiss]\n"))
-#selection = str(raw_input("Do you have 'xterm' and 'expect' installed? [hit 'n' if not or any other key to dismiss]\n"))
-if selection == 'n':
-	print("Please install it now.\n")
-	os.system("sudo apt install xterm")
-#	os.system("sudo apt install expect")
+import sys
 
 terminal_used = 'xterm'    ## may be changd to 'default' - not recommended
 
 slow_pc = 1            # change to 1 if you have PC based on Core 2 Duo etc.
 
-pi_user = 'pi'                # user name on the pi
+pi_user = 'pi'                 # user name on the pi
 ip_first = '10.42.0.'
 port = 22                      # default port used for ssh
 
 pi_pwsd='raspberry'
 
 #phrase=pi_user+"@"+10.42.0.245's password :"
+
+selection = str(raw_input("Do you have 'xterm' installed? \n[hit 'n' if not, 'c' - to cancel or 'Enter' to dismiss]\n"))
+if selection == 'n':
+	print("Please install it now.\n")
+	os.system("sudo apt install xterm")
+#	os.system("sudo apt install expect")
+if selection == 'c':
+	sys.exit()
 
 for i in range (2, 255):
 	print("testing connection with IP "+str(ip_first)+str(i))
