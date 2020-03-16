3100658410b6b61dfc006f6490329c7e0c16efe5
szafranski <p.fabiszewski@gmail.com>
2020-03-16 15:17:02 +0100

    pifind ok

--- pifind.py
+++ pifind.py
@@ -1,12 +1,14 @@
 from time import sleep 
 import os
 
-selection = str(raw_input("Do you have 'xterm' installed? [hit 'n' if not or any other key to dismiss]\n"))
+selection = str(raw_input("Do you have 'xterm' installed? [hit 'n' if not or 'Enter' to dismiss]\n"))
 #selection = str(raw_input("Do you have 'xterm' and 'expect' installed? [hit 'n' if not or any other key to dismiss]\n"))
 if selection == 'n':
 	print("Please install it now.\n")
 	os.system("sudo apt install xterm")
 #	os.system("sudo apt install expect")
+
+terminal_used = 'xterm'    ## may be changd to 'default' - not recommended
 
 pi_user = 'pi'
 ip_first = '10.42.0.'
@@ -17,10 +19,21 @@ pi_pwsd='raspberry'
 #phrase=pi_user+"@"+10.42.0.245's password :"
 
 for i in range (2, 255):
-	print("testing connection with IP "+str(ip_first)+str(i)+" port "+str(port))
-	os.system("xterm -e ssh "+pi_user+"@"+ip_first+str(i)+" port "+str(port)+" &" )
+	print("testing connection with IP "+str(ip_first)+str(i))
+	if terminal_used == 'default':
+		os.system("x-terminal-emulator -e ssh "+pi_user+"@"+ip_first+str(i)+" &")
+	else:
+		os.system("xterm -e ssh "+pi_user+"@"+ip_first+str(i)+" &")
 	if (i % 50) == 0:
-		sleep(5)
+		sleep(2)  # prevens error due to too many xterm instances opened
 		print("checking...")
 		#os.system("kill -9 $(pidof xterm)")
+
+# for i in range (2, 255):
+	# print("testing connection with IP "+str(ip_first)+str(i)+" port "+str(port))
+	# os.system("xterm -e ssh "+pi_user+"@"+ip_first+str(i)+" port "+str(port)+" &" )
+	# if (i % 50) == 0:
+		# sleep(5)
+		# print("checking...")
+		# #os.system("kill -9 $(pidof xterm)")
 
