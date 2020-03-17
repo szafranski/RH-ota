db2d8e37aed1d9703e4ff4da562f71c5c3059849
szafranski <p.fabiszewski@gmail.com>
2020-03-17 19:34:57 +0100

    logging improvements

--- open_scripts.sh
+++ open_scripts.sh
@@ -71,18 +71,22 @@ mkdir log_data
 rm log_data/log.txt > /dev/null 2>&1
 echo > ./log_data/log.txt
 echo FILE /boot/config.txt | tee -a  ./log_data/log.txt
+echo -------------------------------------------- | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 cat /boot/config.txt | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 echo FILE /boot/cmdline.txt | tee -a  ./log_data/log.txt
+echo -------------------------------------------- | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 cat /boot/cmdline.txt | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 echo FILE updater-config.json | tee -a  ./log_data/log.txt
+echo -------------------------------------------- | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 cat ~/RH-ota/updater-config.json | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 echo FILE ~/.ota_markers/ota_config.txt | tee -a  ./log_data/log.txt
+echo -------------------------------------------- | tee -a  ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
 cat ~/.ota_markers/ota_config.txt | tee -a ./log_data/log.txt
 echo | tee -a  ./log_data/log.txt
