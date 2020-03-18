fa53b485690126117efa74d230bb2623927082ae
szafranski <p.fabiszewski@gmail.com>
2020-03-18 18:49:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 sudo apt install python3
 sudo apt install pip
 pip install configparser
-
+printf "\n"
 printf "Installation process completed. You may now open the software with a command 'python3 update.py'.
+printf "\n"
