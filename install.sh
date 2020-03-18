60546f17aa5d574cdbba637562d286025049eeef
szafranski <p.fabiszewski@gmail.com>
2020-03-18 18:48:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Run this scripit with a command 'sh ./install.sh'.
+# Next just open the software using simple 'python3 update.py'.
+
 sudo apt install python3
 sudo apt install pip
 pip install configparser
