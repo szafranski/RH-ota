fd3caa217dd874b461510c8879e3f9338db8a9c0
szafranski <p.fabiszewski@gmail.com>
2020-03-20 18:20:08 +0100

    Add files via upload

--- i2c_nodes_update.py
+++ i2c_nodes_update.py
@@ -12,6 +12,7 @@ else:
         data = json.load(config_file)
 
 RESET_MATE_NODE = 0x79
+DISABLE_SERIAL = 0x80
 
 if data['debug_mode']:
     linux_testing = True
@@ -44,7 +45,11 @@ node6addr = 0x18
 node7addr = 0x20
 node8addr = 0x22
 
-reset_pin = 12
+gpio_reset_pin = 12 #  GPIO pin 12 - nothing to do with nodes pin 12
+
+
+def flash_numbered_node():
+    os.system("sudo avrdude -v -p atmega328p -c arduino -P /dev/ttyS0 -b 57600 -U flash:w:/home/"+user+"/RH-ota/firmware/i2c/reset_no_s.hex:i")
 
 if not linux_testing:
     import RPi.GPIO as GPIO
@@ -52,100 +57,102 @@ if not linux_testing:
     GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
     GPIO.setup(reset_pin, GPIO.OUT, initial=GPIO.HIGH)
 
+    # ensures nothing is being reset during program start
 
-    def all_pins_low():
-        GPIO.output(reset_pin, GPIO.LOW)
-        # GPIO.output(reset_2, GPIO.LOW)
-        # GPIO.output(reset_3,   GPIO.LOW)
-        # GPIO.output(reset_4, GPIO.LOW)
-        # GPIO.output(reset_5, GPIO.LOW)
-        # GPIO.output(reset_6, GPIO.LOW)
-        # GPIO.output(reset_7, GPIO.LOW)
-        # GPIO.output(reset_8, GPIO.LOW)
-        sleep(0.05)
-
-
-    def all_pins_high():
-        GPIO.output(reset_pin, GPIO.HIGH)
-        # GPIO.output(reset_2, GPIO.HIGH)
-        # GPIO.output(reset_3, GPIO.HIGH)
-        # GPIO.output(reset_4, GPIO.HIGH)
-        # GPIO.output(reset_5, GPIO.HIGH)
-        # GPIO.output(reset_6, GPIO.HIGH)
-        # GPIO.output(reset_7, GPIO.HIGH)
-        # GPIO.output(reset_8, GPIO.HIGH)
-        sleep(0.05)
+    def disable_serial_on_all_nodes():
+        bus.write_byte(node1addr, DISABLE_SERIAL)
+        bus.write_byte(node2addr, DISABLE_SERIAL)
+        bus.write_byte(node3addr, DISABLE_SERIAL)
+        bus.write_byte(node4addr, DISABLE_SERIAL)
+        bus.write_byte(node5addr, DISABLE_SERIAL)
+        bus.write_byte(node6addr, DISABLE_SERIAL)
+        bus.write_byte(node7addr, DISABLE_SERIAL)
+        bus.write_byte(node8addr, DISABLE_SERIAL)
 
 
-    def all_pins_reset():
-        GPIO.output(reset_pin, GPIO.LOW)
-        # GPIO.output(reset_2, GPIO.LOW)
-        # GPIO.output(reset_3, GPIO.LOW)
-        # GPIO.output(reset_4, GPIO.LOW)
-        # GPIO.output(reset_5, GPIO.LOW)
-        # GPIO.output(reset_6, GPIO.LOW)
-        # GPIO.output(reset_7, GPIO.LOW)
-        # GPIO.output(reset_8, GPIO.LOW)
+    def gpio_reset_pin_low():
+        GPIO.output(gpio_reset_pin, GPIO.LOW)
         sleep(0.1)
-        GPIO.output(reset_pin, GPIO.HIGH)
-        # GPIO.output(reset_2, GPIO.HIGH)
-        # GPIO.output(reset_3, GPIO.HIGH)
-        # GPIO.output(reset_4, GPIO.HIGH)
-        # GPIO.output(reset_5, GPIO.HIGH)
-        # GPIO.output(reset_6, GPIO.HIGH)
-        # GPIO.output(reset_7, GPIO.HIGH)
-        # GPIO.output(reset_8, GPIO.HIGH)
 
+
+    def gpio_reset_pin_high():
+        GPIO.output(gpio_reset_pin, GPIO.HIGH)
+        sleep(0.1)
+
+
+#  all reset commands have disabling serial on all nodes function implemented - for now
 
     def node_one_reset():
-        all_pins_high()
-        GPIO.output(reset_pin, GPIO.LOW)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node2addr, RESET_MATE_NODE) #  node 2 resets node 1
         sleep(0.1)
-        GPIO.output(reset_pin, GPIO.HIGH)
 
 
     def node_two_reset():
-        bus.write_byte(node1addr, 0x08)
-        # sleep(0.1)
-        # bus.write_byte(node1addr, 0x1)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node1addr, RESET_MATE_NODE) #  node 1 resets node 2
+        sleep(0.1)
 
 
     def node_three_reset():
-        bus.write_byte(node2addr, 0x0)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node4addr, RESET_MATE_NODE) #  node 4 resets node 3
         sleep(0.1)
-        bus.write_byte(node2addr, 0x1)
 
 
     def node_four_reset():
-        bus.write_byte(node3addr, 0x0)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node3addr, RESET_MATE_NODE) #  node 3 resets node 4
         sleep(0.1)
-        bus.write_byte(node3addr, 0x1)
 
 
     def node_five_reset():
-        bus.write_byte(node4addr, 0x0)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node6addr, RESET_MATE_NODE) #  node 6 resets node 5
         sleep(0.1)
-        bus.write_byte(node4addr, 0x1)
 
 
     def node_six_reset():
-        bus.write_byte(node5addr, 0x0)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node5addr, RESET_MATE_NODE) #  node 5 resets node 6
         sleep(0.1)
-        bus.write_byte(node5addr, 0x1)
 
 
     def node_seven_reset():
-        bus.write_byte(node6addr, 0x0)
+        disable_serial_on_all_nodes()
+        bus.write_byte(node8addr, 0x79) #  node 8 resets node 7
         sleep(0.1)
-        bus.write_byte(node6addr, 0x1)
 
 
     def node_eight_reset():
-        bus.write_byte(node7addr, 0x0)
+        bus.write_byte(node7addr, 0x79) #  node 7 resets node 8
         sleep(0.1)
-        bus.write_byte(node7addr, 0x1)
 
 
+
+def flash_all_nodes():
+    input("All nodes will be flashed. Ok? Hit 'Enter'") #  hit enter before start
+    node_one_reset()
+    flash_numbered_node()
+    node_two_reset()
+    flash_numbered_node()
+    node_three_reset()
+    flash_numbered_node()
+    node_four_reset()
+    flash_numbered_node()
+    node_five_reset()
+    flash_numbered_node()
+    node_six_reset()
+    flash_numbered_node()
+    node_seven_reset()
+    flash_numbered_node()
+    node_eight_reset()
+    flash_numbered_node()
+
+
+
+
+## testing leftovers
 
 def flashing():
     input("Ok?")
@@ -158,13 +165,9 @@ def flashing():
     os.system("sudo avrdude -v -p atmega328p -c arduino -P /dev/ttyS0 -b 57600 -U flash:w:/home/"+user+"/RH-ota/firmware/reset_no_s.hex:i")
     sleep(1)
 
-
-flashing()
+#flashing()
 
 #  sudo avrdude -v -p atmega328p -c arduino -P /dev/ttyS0 -b 57600 -U flash:w:/home/pi/RH-ota/firmware/reset_no_s.hex:i
-
-
-
 
 def test():
     selection = input("What do you want to send?")
@@ -183,5 +186,6 @@ def test():
     if selection == '3':
         sys.exit()
 
-
 #test()
+
+
