0c06354e99cc2980330b6a595cdbe6526a3d8c13
szafranski <p.fabiszewski@gmail.com>
2020-04-08 22:05:29 +0200

    Update self.py

--- self.py
+++ self.py
@@ -33,7 +33,7 @@ def self_update(config):
         os.system(f"./scripts/self_updater.sh {source}")
         # if config:  # if the config variable is not empty, then the config file must have existed.
             # shutil.copyfile("~/.ota_markers/old_RH-ota/updater-config.json", "~/RH-ota/updater-config.json")
-        new_version_name = get_ota_version(config)
+        new_version_name = get_ota_version('update')
         print(f"""
     
     RotorHazard OTA Manager updated to version {new_version_name}
@@ -42,7 +42,7 @@ def self_update(config):
 
             """)
         sleep(1)
-        make_directories_accessible('update')
+        make_directories_accessible(config)
         os.system("cp ~/.ota_markers/old_RH-ota/updater-config.json ~/RH-ota/updater-config.json")
         # it had some bug with shutil - can be changed when resolved
         if new_version_name != old_version_name:
