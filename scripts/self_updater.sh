b97cfa5d69e08aec7a3e4b58754bfcf527fe3c3c
szafranski <p.fabiszewski@gmail.com>
2020-04-08 15:57:35 +0200

    self updater tweaks - whole folder copying

--- scripts/self_updater.sh
+++ scripts/self_updater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -rf ~/RH-ota*
-wget https://codeload.github.com/szafranski/RH-ota/zip/"${1}" -O tempota.zip
 rm tempota.zip >/dev/null >/dev/null 2>&1
+wget https://codeload.github.com/szafranski/RH-ota/zip/"${1}" -O tempota.zip
 unzip tempota.zip
 rm tempota.zip
 mv RH-ota-* RH-ota
