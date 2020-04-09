5919d0dad464ce681254801a6827ef88a922f8fe
szafranski <p.fabiszewski@gmail.com>
2020-04-09 11:44:38 +0200

    ota.sh installs without confirmation

--- ota.sh
+++ ota.sh
@@ -15,35 +15,35 @@ echo installing dependencies may need 'sudo'
 
 which python3 >/dev/null
 if [ $? -gt 0 ]; then
-  echo python3 has to be installed && sudo apt install python3
+  echo python3 has to be installed && sudo apt install python3 -y
 else
   echo python3"    "found # those  cure my ocd
 fi
 
 which avrdude >/dev/null
 if [ $? -gt 0 ]; then
-  echo avrdude has to be installed && sudo apt install avrdude
+  echo avrdude has to be installed && sudo apt install avrdude -y
 else
   echo avrdude"    "found
 fi
 
 which curl >/dev/null
 if [ $? -gt 0 ]; then
-  echo curl has to be installed && sudo apt install curl
+  echo curl has to be installed && sudo apt install curl -y
 else
   echo curl"       "found
 fi
 
 which cowsay >/dev/null
 if [ $? -gt 0 ]; then
-  echo cowsay has to be installed && sudo apt install cowsay
+  echo cowsay has to be installed && sudo apt install cowsay -y
 else
   echo cowsay"     "found
 fi
 
 which pip3 >/dev/null
 if [ $? -gt 0 ]; then
-  echo pip3 package has to be installed && sudo apt install python3-pip
+  echo pip3 package has to be installed && sudo apt install python3-pip -y
 else
   echo pip3"       "found
 fi
@@ -58,43 +58,43 @@ dots7 & apt list 2>/dev/null | tee aptinstalled.tmp >/dev/null
 if grep -q 'python3-gpiozero' aptinstalled.tmp; then
   echo python3-gpiozero" "found
 else
-  echo echo python3-gpiozero has to be installed && sudo apt install python3-gpiozero
+  echo echo python3-gpiozero has to be installed && sudo apt install python3-gpiozero -y
 fi
 
 if grep -q 'python3-requests' aptinstalled.tmp; then
   echo python3-requests" "found
 else
-  echo echo python3-requests has to be installed && sudo apt install python3-requests
+  echo echo python3-requests has to be installed && sudo apt install python3-requests -y
 fi
 
 if grep -q 'python3-dev' aptinstalled.tmp; then
   echo python3-dev"      "found
 else
-  echo echo python3-dev has to be installed && sudo apt install python3-dev
+  echo echo python3-dev has to be installed && sudo apt install python3-dev -y
 fi
 
 if grep -q 'python3-rpi.gpio' aptinstalled.tmp; then
   echo python3-rpi.gpio" "found
 else
-  echo echo python3-rpi.gpio has to be installed && sudo apt install python3-rpi.gpio || echo - only on Pi -
+  echo echo python3-rpi.gpio has to be installed && sudo apt install python3-rpi.gpio  -y|| echo - only on Pi -
 fi
 
 if grep -q 'python3-smbus' aptinstalled.tmp; then
   echo python3-smbus"    "found
 else
-  echo echo python3-smbus has to be installed && sudo apt install python3-smbus
+  echo echo python3-smbus has to be installed && sudo apt install python3-smbus -y
 fi
 
 if grep -q 'i2c-tools' aptinstalled.tmp; then
   echo i2c-tools"        "found
 else
-  echo echo i2c-tools has to be installed && sudo apt install python3-smbus
+  echo echo i2c-tools has to be installed && sudo apt install i2c-tools -y
 fi
 
 if grep -q 'zip' aptinstalled.tmp; then
   echo zip"              "found
 else
-  echo echo zip has to be installed && sudo apt install zip unzip # todo does it check for unzip too?
+  echo echo zip has to be installed && sudo apt install zip unzip  -y # todo does it check for unzip too?
 fi
 
 # Cleanup after myself.
