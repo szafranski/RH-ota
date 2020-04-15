38803c0b4658b5c8877650e4df53246a18539444
szafranski <p.fabiszewski@gmail.com>
2020-04-16 00:00:01 +0200

    pi 4 detection tests

---
+++ .dev/pi4.sh
@@ -0,0 +1,9 @@
+ifs=':' read -ra piversion <<< "$(cat /proc/cpuinfo | grep Revision)"
+if [ ${piversion[2]} == *"03111"*  ] ; then
+  echo "Pi 4 detected - no errors" || echo " -- error! -- "
+else
+  echo "This is not Pi 4 - no errors" || echo " -- error -- "
+fi
+
+echo "  Thanks Casey!"
+
