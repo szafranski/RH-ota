06669d42faeec0243e144c19adcd15f8a086a9ad
szafranski <p.fabiszewski@gmail.com>
2020-04-17 03:00:45 +0200

    caseys script

--- .dev/isPi4.sh
+++ .dev/isPi4.sh
@@ -1,5 +1,5 @@
 ifs=':' read -ra piversion <<< "$(cat /proc/cpuinfo | grep Revision)"
-if [ ${piversion[2]} == *"03111"*  ] ; then
+if [[ ${piversion[2]} == *"03111"*  ]] ; then
   echo "Pi 4 detected - no errors" || echo " -- error! -- "
 else
   echo "This is not Pi 4 - no errors" || echo " -- error -- "
