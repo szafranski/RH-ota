7a4f0f8ead080a7fb249f91af470f5e5a1890e5b
szafranski <p.fabiszewski@gmail.com>
2020-04-21 22:54:59 +0200

    pi 4 test ok

--- .dev/isPi4.sh
+++ .dev/isPi4.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 ifs=':' read -ra piversion <<< "$(cat /proc/cpuinfo | grep Revision)"
-if [[ ${piversion[2]} == *"03111"*  ]] ; then
-  echo "Pi 4 detected - no errors" || echo " -- error! -- "
+if [[ ${piversion[2]} == *"0311"* ]] ; then
+echo "Pi 4 detected - no errors" || echo " -- error 1 -- "
 else
-  echo "This is not Pi 4 - no errors" || echo " -- error -- "
+echo "This is not Pi 4 - no errors" || echo " -- error 2 -- "
 fi
 
-echo "  Thanks Casey!  ￼ "
+echo " Thanks for helping!   "
