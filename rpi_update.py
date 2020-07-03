72384bb4f9766f9be49c2b75ffb5a08cdc96747f
szafranski <p.fabiszewski@gmail.com>
2020-07-03 12:03:47 +0200

    Update rpi_update.py

--- rpi_update.py
+++ rpi_update.py
@@ -8,7 +8,7 @@ from modules import clear_the_screen, Bcolors, triangle_image_show, internet_che
 
 
 def check_preferred_rh_version(config):
-    stable_release_name = '2.1.1'  # declare last stable release name here
+    stable_release_name = '2.2.0'  # declare last stable release name here
     beta_release_name = '2.2.0-beta.3'  # declare last beta release name here
 
     if config.rh_version == 'stable':
