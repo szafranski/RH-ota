f6b20539277069e26dfb308f4972023f35632c0f
szafranski <p.fabiszewski@gmail.com>
2021-01-09 18:18:34 +0100

    Update serv_check.sh

--- scripts/serv_check.sh
+++ scripts/serv_check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-FILE=/lib/systemd/system/rotorhazard.service
+SERVICE_FILE=/lib/systemd/system/rotorhazard.service
 
-old_python_statement="ExecStart=/usr/bin/python server.py"
+old_python_service_statement="ExecStart=/usr/bin/python server.py"
 
 
-if test -f "$FILE"; then
+if test -f "$SERVICE_FILE"; then
 
-if grep -Fxq "$old_python_statement" "$FILE"; then
+if grep -Fxq "$old_python_service_statement" "$SERVICE_FILE"; then
     echo "old python based RotorHazard autostart service found"
-    sed -i 's/python/python3/g' "$FILE"
+    sudo sed -i 's/python/python3/g' "$SERVICE_FILE"
     echo "changed to python3 based service"
 else
     echo "RotorHazard autostart service is up to date"
@@ -18,5 +18,11 @@ else
     echo "no RotorHazard autostart service found - no changes"
 fi
 
-# below ensures that if user had "server start" alias added it will be updated as well
-sed -i 's/python server.py/python3 server.py/g' ~/.bashrc
+
+
+if grep -Fxq "python server.py" "/home/"$USER"/.bashrc"; then
+    echo "old python based server-start alias found"
+    sed -i 's/python server.py/python3 server.py/g' ~/.bashrc
+    echo "changed to python3 based alias"
+fi
+
