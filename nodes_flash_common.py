7403833b7ed8ff03d72cbc86aa345433808c79a2
szafranski <pawel.fabiszewski@trecom.pl>
2021-05-02 11:55:28 +0200

    exp_2

--- nodes_flash_common.py
+++ nodes_flash_common.py
@@ -85,7 +85,7 @@ def prepare_mate_node(addr):
 
 
 def flash_mate_node(config, firmware_version):
-    avrdude_action = f"avrdude -v -p atmega328p -c arduino -P /dev/{config.port_name} -b 57600 -U \
+    avrdude_action = f"avrdude -v -p atmega328p -c arduino -P /dev/{config.port_name} -b 115200 -U \
     flash:w:/home/{config.user}/RH-ota/firmware/{firmware_version}.hex:i"
     os.system(f"{avrdude_action}")
 
