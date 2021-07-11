d4f511e8c2d3546d2aa14ccd995cbb8000301da3
szafranski <pawel.fabiszewski@trecom.pl>
2021-07-11 18:40:51 +0200

    oled screen support added

--- resources/oled_screen/oled_screen.py
+++ resources/oled_screen/oled_screen.py
@@ -22,7 +22,7 @@
 # https://www.the-diy-life.com/connect-and-program-an-oled-stats-display-for-your-raspberry-pi/
 
 from time import sleep
-#import Adafruit_GPIO.SPI as SPI
+# import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
 from PIL import Image
@@ -32,97 +32,103 @@ from PIL import ImageFont
 import subprocess
 import psutil
 
-# Raspberry Pi pin configuration:
-RST = None  # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-#DC = 23
-#SPI_PORT = 0
-#SPI_DEVICE = 0
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
+def load_oled_screen():
+    # Raspberry Pi pin configuration:
+    RST = None  # on the PiOLED this pin isnt used
+    # Note the following are only used with SPI:
+    # DC = 23
+    # SPI_PORT = 0
+    # SPI_DEVICE = 0
 
-# 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+    # Beaglebone Black pin configuration:
+    # RST = 'P9_12'
+    # Note the following are only used with SPI:
+    # DC = 'P9_15'
+    # SPI_PORT = 1
+    # SPI_DEVICE = 0
 
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+    # 128x32 display with hardware I2C:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+    # 128x64 display with hardware I2C:
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+    # Note you can change the I2C address by passing an i2c_address parameter like:
+    # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
 
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+    # Alternatively you can specify an explicit I2C bus number, for example
+    # with the 128x32 display you would use:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
 
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+    # 128x32 display with hardware SPI:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+    # 128x64 display with hardware SPI:
+    # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
-# Initialize library.
-disp.begin()
+    # Alternatively you can specify a software SPI implementation by providing
+    # digital GPIO pin numbers for all the required display pins.  For example
+    # on a Raspberry Pi with the 128x32 display you might use:
+    # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
-# Clear display.
-disp.clear()
-disp.display()
+    # Initialize library.
+    disp.begin()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    # Clear display.
+    disp.clear()
+    disp.display()
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 0
-top = padding
-bottom = height - padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-
-# Load default font.
-font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-
-while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    cmd = "hostname -I | cut -d\' \' -f1"
-    ip_addr = subprocess.check_output(cmd, shell=True)
-    cmd = "hostname"
-    hostname = subprocess.check_output(cmd, shell=True)
-    cpu_usage = str(psutil.cpu_percent()).split(".")[0]
-    raw_temp = subprocess.check_output("vcgencmd measure_temp |cut -f 2 -d '='", shell=True)
-    temp = str(raw_temp).split(".")[0].split("\"")[1]
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = 0
+    top = padding
+    bottom = height - padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
 
-    draw.text((x, top), "IP: " + str(ip_addr, 'utf-8'), font=font, fill=255)
-    draw.text((x, top + 16), str(hostname, 'utf-8'), font=font, fill=255)
-    draw.text((x, top + 32), str(f"CPU: {cpu_usage}%"), font=font, fill=255)
-    draw.text((64, top + 32), str(f"TEMP: {temp}*C"), font=font, fill=255)
-    draw.text((x, top + 48), str(f"Temp: {temp}*C"), font=font, fill=255)
+    # Load default font.
+    font = ImageFont.load_default()
 
-    # Display image.
-    disp.image(image)
-    disp.display()
-    sleep(0.3)
+    # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    # font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+    while True:
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+        cmd = "hostname -I | cut -d\' \' -f1"
+        ip_addr = subprocess.check_output(cmd, shell=True)
+        cmd = "hostname"
+        hostname = subprocess.check_output(cmd, shell=True)
+        cpu_usage = str(psutil.cpu_percent()).split(".")[0]
+        raw_temp = subprocess.check_output("vcgencmd measure_temp |cut -f 2 -d '='", shell=True)
+        temp = str(raw_temp).split(".")[0].split("\"")[1]
+
+        draw.text((x, top), "IP: " + str(ip_addr, 'utf-8'), font=font, fill=255)
+        draw.text((x, top + 16), str(hostname, 'utf-8'), font=font, fill=255)
+        draw.text((x, top + 32), str(f"CPU: {cpu_usage}%"), font=font, fill=255)
+        draw.text((64, top + 32), str(f"TEMP: {temp}*C"), font=font, fill=255)
+        draw.text((x, top + 48), str(f"Temp: {temp}*C"), font=font, fill=255)
+
+        # Display image.
+        disp.image(image)
+        disp.display()
+        sleep(0.3)
+
+
+if __name__ == "__main__":
+    load_oled_screen()
