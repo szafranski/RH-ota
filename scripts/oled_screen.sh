6bad561f0a76998f71e41aba6867c2f0b954f828
szafranski <pawel.fabiszewski@trecom.pl>
2021-07-13 00:42:50 +0200

    added oled screen installation script

---
+++ scripts/oled_screen.sh
@@ -0,0 +1,6 @@
+cd ~
+wget https://codeload.github.com/adafruit/Adafruit_Python_SSD1306/zip/master -O oled_screen.zip
+unzip oled_screen.zip
+cd Adafruit_Python_SSD1306-master
+sudo python3 setup.py install
+sudo -H pip3 install Adafruit_BBIO
