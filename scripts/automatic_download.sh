8cb55fad72680e687e4b98fee94d539285e049b1
szafranski <pawel.fabiszewski@trecom.pl>
2022-11-29 13:46:06 +0100

    automatic download - 1st try

--- scripts/automatic_download.sh
+++ scripts/automatic_download.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-cd ~
-wget https://codeload.github.com/szafranski/RH-ota/zip/stable -O tempota.zip
-unzip tempota.zip
-rm tempota.zip
-mv RH-ota-* RH-ota
+script(){
+cd ~ || return 1
+wget https://codeload.github.com/szafranski/RH-ota/zip/stable -O tempota.zip || return 1
+unzip tempota.zip || return 1
+rm tempota.zip || return 1
+mv RH-ota-* RH-ota || return 1
+cd RH-ota || return 1
+
+echo "
+
+Program downloaded successfully. To open the program now type:
+
+cd RH-ota
+./ota.sh
+
+"
+
+}
+
+script || echo "
+
+errors encountered - try manual installation -> more info od github
+
+"
+
