c036bc7076b95d174f689a888fa0c873c4b328ab
pfabi <p.fabiszewski@gmail.com>
2023-02-10 21:27:01 +0100

    RotorHazard 3.2.0-beta.4 installation and update support added

--- scripts/auto_download.sh
+++ scripts/auto_download.sh
@@ -11,7 +11,7 @@ script(){
 cd ~ || return 1
 mv RH-ota* RH-ota.old >> /dev/null 2&>1           #TODO in case of non-empty directories
 mv tempota.zip tempota.zip.old >> /dev/null 2&>1  #TODO in case of non-empty directories
-wget https://codeload.github.com/szafranski/RH-ota/zip/stable -O tempota.zip || return 1
+wget https://codeload.github.com/szafranski/RH-ota/zip/main -O tempota.zip || return 1
 unzip tempota.zip || return 1
 rm tempota.zip || return 1
 mv RH-ota-* RH-ota || return 1
